a2da5fa2eb597ddf2e6ff4b9b92d5440ff79e0de
Nafis <nafisbiswas10@gmail.com>
2026-05-14 02:55:39 +0800

    Added migrate to innit, enable migration after any changes to database (added overscore field to db)

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_migrate import Migrate
 from config import Config
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
@@ -12,6 +14,7 @@ def create_app():
 
     db.init_app(app)
     login_manager.init_app(app)
+    migrate.init_app(app, db)
     login_manager.login_view = 'auth.login'
 
     from app.routes.main import main
