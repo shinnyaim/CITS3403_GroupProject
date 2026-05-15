0c64312810a369346450e64174168487a185634d
Jo <jozellealmero@gmail.com>
2026-05-16 02:19:13 +0800

    created multiple configurations

--- config.py
+++ config.py
@@ -1,7 +1,16 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-change-this'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///game.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+class DeploymentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, 'test.db')
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+    TESTING = True
+    WTF_CSRF_ENABLED = False
