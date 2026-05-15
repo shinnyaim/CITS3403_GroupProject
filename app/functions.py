6e4e83e1c7d9d4ce220e89ba41bdaa1e8e55bdbb
Jo <jozellealmero@gmail.com>
2026-05-16 03:45:54 +0800

    created a function to check whether credentials match

--- app/functions.py
+++ app/functions.py
@@ -3,3 +3,6 @@ def passwordsMatch(password, confirmPassword):
 
 def isValidEmail(email):
     return "@" in email and "." in email.split("@"[-1])
+
+def checkCreds(storedPW, inputPw):
+    return storedPW == inputPw
