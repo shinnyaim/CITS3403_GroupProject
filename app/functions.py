8a15a893504f5d58afecf52a0595d816f4e49030
Jo <jozellealmero@gmail.com>
2026-05-16 15:03:26 +0800

    fixed error in isValidEmail

--- app/functions.py
+++ app/functions.py
@@ -2,7 +2,7 @@ def passwordsMatch(password, confirmPassword):
     return password == confirmPassword
 
 def isValidEmail(email):
-    return "@" in email and "." in email.split("@"[-1])
+    return "@" in email and "." in email.split("@")[-1]
 
 def checkCreds(storedPW, inputPw):
     return storedPW == inputPw
