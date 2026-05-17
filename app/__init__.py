c3cb03cfababd85fafc7e1ee0466122524e0a090
Shin <24436991@student.uwa.edu.au>
2026-05-17 17:34:51 +0800

    import CSRFProtect

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect
 from config import Config
 
 db = SQLAlchemy()
 
 login_manager = LoginManager()
 migrate = Migrate()
+csrf = CSRFProtect()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -16,6 +18,8 @@ def create_app(config_class=Config):
     db.init_app(app)
     login_manager.init_app(app)
     migrate.init_app(app, db)
+    csrf.init_app(app)
+    
     login_manager.login_view = 'auth.login'
 
     from app.routes.main import main
